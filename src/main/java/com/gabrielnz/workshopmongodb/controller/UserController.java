dc3c52990f097a165d39c32d8e27154b206445d2
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2025-05-08 14:26:16 -0300

    Using UserDTO

--- src/main/java/com/gabrielnz/workshopmongodb/controller/UserController.java
+++ src/main/java/com/gabrielnz/workshopmongodb/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.gabrielnz.workshopmongodb.controller;
 
 import com.gabrielnz.workshopmongodb.domain.User;
+import com.gabrielnz.workshopmongodb.dto.UserDTO;
 import com.gabrielnz.workshopmongodb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,8 +19,9 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> userDTOList = list.stream().map(x -> new UserDTO(x)).toList();
+        return ResponseEntity.ok().body(userDTOList);
     }
 }
---
+++ src/main/java/com/gabrielnz/workshopmongodb/dto/UserDTO.java
@@ -0,0 +1,44 @@
+package com.gabrielnz.workshopmongodb.dto;
+
+import com.gabrielnz.workshopmongodb.domain.User;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+    private String name;
+    private String password;
+    private String email;
+
+    public UserDTO() {
+
+    }
+    public UserDTO(User user) {
+        this.name = user.getName();
+        this.password = user.getPassword();
+        this.email = user.getEmail();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
