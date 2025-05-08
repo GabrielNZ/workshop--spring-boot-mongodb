274b81b19ce78eaacc85a8a9bb2ee0c9b410df2a
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2025-05-08 13:53:34 -0300

    Conection MongoDB with repository and service

--- src/main/java/com/gabrielnz/workshopmongodb/controller/UserController.java
+++ src/main/java/com/gabrielnz/workshopmongodb/controller/UserController.java
@@ -1,25 +1,24 @@
 package com.gabrielnz.workshopmongodb.controller;
 
 import com.gabrielnz.workshopmongodb.domain.User;
+import com.gabrielnz.workshopmongodb.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
 
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User user1 = new User("1","Maria","123456","maria@gmail.com");
-        User user2 = new User("2","Alex","123456","alex@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(user1,user2));
+        List<User> list = userService.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
